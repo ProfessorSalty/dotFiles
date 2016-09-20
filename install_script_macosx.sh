43ad05d618a2a7309a31c06920b5667396e550bb
Gregory Smith <crashingwaves@fea.st>
2016-09-20 08:49:18 -0600

    Install sass_lint

--- install_script_macosx.sh
+++ install_script_macosx.sh
@@ -26,7 +26,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 #Install globals for Sublime Text plugins
-gem install rubocop haml
+gem install rubocop haml scss_lint
 
 pip install powerline-status
 
