21a3c71833e03d95a93ee9991b37ac0c660cd312
PortableStick <crashingwaves@fea.st>
2017-05-15 00:23:46 -0600

    Fixes

--- install_script_ubuntu.sh
+++ install_script_ubuntu.sh
@@ -3,7 +3,7 @@
 # TODO
 # Colorize the output
 DOTFILES=$HOME/.dotFiles/
-CODE=$(which code)
+sudo apt-get install -y git aptitude
 #Get the information we need first, if we need it
 if  [ -z "$(git config --global user.name)" ]; then
     echo "Please enter your name (for git config): "
@@ -25,10 +25,19 @@ if [ -z "$(which dotnet)" ]; then
     sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 fi
+#Heroku toolbelt
+sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+echo *************************************************
+#hub
+sudo add-apt-repository ppa:cpick/hub -y
 
-sudo add-apt-repository ppa:cpick/hub
-sudo apt-get update
-sudo apt-get install hub gnupg tidy-html5 gpg-agent mongodb tmux zsh autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev tree imagemagick shellcheck postgres zeromq pyqt gcc numpy scipy mysql heroku-toolbelt redis libzmq-dev code 'dotnet-dev-*' guake
+sudo aptitude update
+# Have to use apt-get here
+sudo apt-get install 'dotnet-dev-*'
+sudo aptitude install -y hub gnupg tidy gnupg-agent mongodb tmux zsh autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev tree imagemagick shellcheck postgresql postgresql-contrib python-qt4 gcc python-numpy python-scipy python-matplotlib ipython python-pandas python-sympy python-nose mysql-server heroku redis-server redis-tools code tilda python-setuptools python-dev
+sudo easy_install pip
+
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 git clone https://github.com/nodenv/nodenv.git ~/.nodenv
@@ -62,6 +71,7 @@ pip install --upgrade distribute
 pip install --upgrade pip
 
 pip install venv
+pip install --upgrade virtualenv
 pip install ipython[zmq,qtconsole,notebook,test]
 
 echo "Installing NPM modules..."
@@ -98,6 +108,16 @@ for FILEPATH in $DOTFILES/rcfiles/*; do
     ln -s $FILEPATH ~/.$FILENAME
 done
 
+for FILEPATH in $DOTFILES/zsh/*; do
+    FILENAME=${FILEPATH##*/}
+    echo "Linking $FILENAME...";
+    if [ -L ~/.$FILENAME ]; then
+        rm ~/.$FILENAME
+        echo "Removing $FILENAME..."
+    fi
+    ln -s $FILEPATH ~/.$FILENAME
+done
+
 if [ ! -d ~/.config/powerline/themes/tmux ]; then
     echo "Linking tmux powerline theme file..."
     mkdir -p ~/.config/powerline/themes/tmux
@@ -115,27 +135,6 @@ echo "Linking zshrc..."
 if [ -f ~/.zshrc ]; then
     rm ~/.zshrc
 fi
-ln -s $DOTFILES/zsh/zshrc ~/.zshrc
-echo "Linking zprofile..."
-if [ -f ~/.zprofile ]; then
-    rm ~/.zprofile
-fi
-ln -s $DOTFILES/zsh/zprofile ~/.zprofile
-echo "Linking zlogin..."
-if [ -f ~/.zlogin ]; then
-    rm ~/.zlogin
-fi
-ln -s $DOTFILES/zsh/zlogin ~/.zlogin
-echo "Linking zpath..."
-if [ -f ~/.zpath ]; then
-    rm ~/.zpath
-fi
-ln -s $DOTFILES/zsh/zpath ~/.zpath
-echo "Linking zshenv..."
-if [ -f ~/.zshenv ]; then
-    rm ~/.zshenv
-fi
-ln -s $DOTFILES/zsh/zshenv ~/.zshenv
 
 # setup vim
 if [ -f ~/.vim ]; then
@@ -153,7 +152,7 @@ if [ -f $HOME/.config/Code/User/settings.json ]; then
     rm $HOME/.config/Code/User/settings.json
 fi
 ln -s $DOTFILES/vscode/settings.json $HOME/.config/Code/User/
-
+CODE=$(which code)
 if [ -f /usr/local/bin/code ]; then
     rm /usr/local/bin/code
 fi
@@ -165,31 +164,18 @@ for X in $(< $DOTFILES/vscode/vscode-extensions.txt); do
     $CODE --install-extension $X
 done
 
-# Atom
-# echo "Installing Atom and saved settings..."
-# curl -LSso https://github.com/atom/atom/releases/latest $HOME/Downloads/atom
-# ln -s $DOTFILES/backup_atom.sh /usr/local/bin/backup_atom
-# ln -s $DOTFILES/atom/config.cson $HOME/.atom/
-# for X in $(< $DOTFILES/atom/atom-extensions.txt); do
-#     apm install $X
-# done
-
-# Other stuff
+# RSA
 if [  ! -d ~/.ssh ] || [ ! -f ~/.ssh/id_rsa ]; then
     echo "Generating RSA keypair - you'll need to enter a passphrase..."
     ssh-keygen -t rsa -b 4096
 fi
 
+# setup mysql
 if [ ! -d /usr/local/var/mysql ]; then
     echo "Setting up MySQL...."
     unset TMPDIR
     mkdir /usr/local/var
-    mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
-fi
-
-if [ ! -d /usr/local/var/postgres ]; then
-    echo "Setting up PostGres...."
-    postgres -D /usr/local/var/postgres
+    sudo mysql_secure_installation
 fi
 
 # set zsh as default
@@ -199,3 +185,4 @@ mkdir -p ~/Projects
 chmod +x $DOTFILES/setup_mac.sh $DOTFILES/backup_vscode.sh $DOTFILES/backup_atom.sh $DOTFILES/backup_editors.sh
 ln -s $DOTFILES/backup_editors.sh /usr/local/bin/backup_editors
 echo "Install and setup complete.  Now run the setup script."
+
