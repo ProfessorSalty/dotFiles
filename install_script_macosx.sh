2531cc9a98ca8ab7980897f87679f6425f6269cf
Gregory Smith <crashingwaves@fea.st>
2017-08-09 17:01:05 -0600

    Updates

--- install_script_macosx.sh
+++ install_script_macosx.sh
@@ -241,5 +241,16 @@ git config --global core.excludesfile ~/.gitignore_global
 export $GOPATH=$HOME/Projects/go
 mkdir -p ~/Projects/go/bin
 go get -u github.com/nsf/gocode
+go get -u github.com/ramya-rao-a/go-outline
+go get -u github.com/tpng/gopkgs
+go get -u github.com/acroca/go-symbols
+go get -u golang.org/x/tools/cmd/guru
+go get -u golang.org/x/tools/cmd/gorename
+go get -u github.com/fatih/gomodifytags
+go get -u github.com/josharian/impl
+go get -u github.com/rogpeppe/godef
+go get -u sourcegraph.com/sqs/goreturns
+go get -u github.com/golang/lint/golint
+go get -u github.com/cweill/gotests/...
 ln -s $DOTFILES/backup_editors.sh /usr/local/bin/backup_editors
 echo "Install and setup complete.  Now run the setup script."
