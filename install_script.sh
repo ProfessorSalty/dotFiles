8f2e06e449a41cb83ddaca1c306410ae56e0cee2
Gregory Smith <crashingwaves@fea.st>
2017-08-23 17:19:40 -0600

    Updates

--- install_script.sh
+++ install_script.sh
@@ -4,7 +4,8 @@
 # Colorize the output
 DOTFILES=$HOME/.dotFiles/
 XDG_CONFIG_HOME=$HOME/.config
-export GOPATH=$HOME/Projects/go
+GOPATH=$HOME/Projects/go
+export GOPATH
 
 OS="unknown"
 case "$OSTYPE" in
