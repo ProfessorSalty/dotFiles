190ffbac226b43c7dbbbfe90869b5978efd255d8
Gregory Smith <crashingwaves@fea.st>
2017-10-22 11:32:37 -0600

    Updates

--- install_script.sh
+++ install_script.sh
@@ -69,8 +69,9 @@ elif [ "$DISTRO" == "UBUNTU" ]; then
     echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
     sudo wget -qO "$GPGKEY/sublimehq-pub.gpg" https://download.sublimetext.com/sublimehq-pub.gpg
     # for Heroku
-    echo "deb https://cli-assets.heroku.com/branches/stable/apt ./" > /etc/apt/sources.list.d/heroku.list
-    sudo wget -qO "$GPGKEY/heroku-release.key" https://cli-assets.heroku.com/apt/release.key
+    #echo "deb https://cli-assets.heroku.com/branches/stable/apt ./" > /etc/apt/sources.list.d/heroku.list
+    #sudo wget -qO "$GPGKEY/heroku-release.key" https://cli-assets.heroku.com/apt/release.key
+    wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
     # nexcloud client
     sudo add-apt-repository -y ppa:nextcloud-devs/client
     apt-get update
