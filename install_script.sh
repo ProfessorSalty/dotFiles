d3f9f70587e154db4260a0ab1b30f9de8e005b8c
Gregory Smith <crashingwaves@fea.st>
2017-10-22 11:17:01 -0600

    Updates

--- install_script.sh
+++ install_script.sh
@@ -74,12 +74,11 @@ elif [ "$DISTRO" == "UBUNTU" ]; then
     # nexcloud client
     sudo add-apt-repository ppa:nextcloud-devs/client
     apt-get update
-    sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev neovim python-neovim python3-neovim tmux zsh python postgresql postgresql-contrib tcl shellcheck golang-go mysql-server heroku python3-pip tree feh rofi xbacklight pulseaudio-utils compton xfce4-power-manager nextcloud-client rxvt-unicode neofetch geary apt-transport-https
+    sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev neovim python-neovim python3-neovim tmux zsh python postgresql postgresql-contrib tcl shellcheck  heroku python3-pip tree feh rofi xbacklight pulseaudio-utils compton xfce4-power-manager nextcloud-client rxvt-unicode neofetch geary apt-transport-https
     # install submlime text
     sudo apt-get install -y sublime-text
     # Go from snaps
     sudo snap install --classic go
-    sudo mysql_secure_installation
     # build hub
     git clone https://github.com/github/hub.git
     cd hub || return
@@ -361,4 +360,8 @@ fi
 git config --global credential.helper osxkeychain
 git config --global core.excludesfile ~/.gitignore_global
 #make note to use mysql_secure_installation
+if [ "$DISTRO" == "UBUNTU" ]; then
+    sudo apt-get install mysql-server
+    sudo mysql_secure_installation
+fi
 echo "Install and setup complete.  Now run the setup script."
