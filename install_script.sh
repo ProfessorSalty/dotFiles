f63d4d2492c5bf53104a18a73918a83c8d7302eb
Gregory Smith <crashingwaves@fea.st>
2017-10-22 11:25:41 -0600

    Updates

--- install_script.sh
+++ install_script.sh
@@ -67,12 +67,12 @@ if [ $OS == "MAC" ]; then
 elif [ "$DISTRO" == "UBUNTU" ]; then
     # for Sublime Text
     echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-    sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg -O "$GPGKEY/sublimehq-pub.gpg"
+    sudo wget -qO "$GPGKEY/sublimehq-pub.gpg" https://download.sublimetext.com/sublimehq-pub.gpg
     # for Heroku
     echo "deb https://cli-assets.heroku.com/branches/stable/apt ./" > /etc/apt/sources.list.d/heroku.list
-    sudo wget -qO- https://cli-assets.heroku.com/apt/release.key -O "$GPGKEY/heroku-release.gpg"
+    sudo wget -qO "$GPGKEY/heroku-release.gpg" https://cli-assets.heroku.com/apt/release.key
     # nexcloud client
-    sudo add-apt-repository ppa:nextcloud-devs/client
+    sudo add-apt-repository -y ppa:nextcloud-devs/client
     apt-get update
     sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev neovim python-neovim python3-neovim tmux zsh python postgresql postgresql-contrib tcl shellcheck  heroku python3-pip tree feh rofi xbacklight pulseaudio-utils compton xfce4-power-manager nextcloud-client rxvt-unicode neofetch geary apt-transport-https
     # install submlime text
