ab2c331a8c9a0eefad8858390a5ac378eb8dc3e7
Gregory Smith <crashingwaves@fea.st>
2017-10-23 19:46:38 -0600

    Fixed zprofile; nativescript borked

--- install_script.sh
+++ install_script.sh
@@ -68,7 +68,7 @@ if [ $OS == "MAC" ]; then
 elif [ "$DISTRO" == "UBUNTU" ]; then
     # for Sublime Text
     echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-    sudo wget -qO "$GPGKEY/sublimehq-pub.gpg" https://download.sublimetext.com/sublimehq-pub.gpg
+    sudo wget -qO -  https://download.sublimetext.com/sublimehq-pub.gpg
     # for Heroku
     #echo "deb https://cli-assets.heroku.com/branches/stable/apt ./" > /etc/apt/sources.list.d/heroku.list
     #sudo wget -qO "$GPGKEY/heroku-release.key" https://cli-assets.heroku.com/apt/release.key
